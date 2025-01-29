6f99b207a5ef8ba1eaf76d0b953e6b3d3d755e7a
JAbdelEsp <jabdelespinosa@Juans-MacBook-Pro.local>
2025-01-28 22:43:58 -0600

    fix errors

--- backend/schemas/newsSchema.js
+++ backend/schemas/newsSchema.js
@@ -1,24 +1,13 @@
 const newsSchema = `
-    CREATE TABLE `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_news` varchar(100) NOT NULL,
-  `title` varchar(10000) DEFAULT NULL,
-  `author` varchar(1000) DEFAULT NULL,
-  `content` longtext DEFAULT NULL,
-  `date` varchar(500) DEFAULT NULL,
-  `user` varchar(1000) DEFAULT NULL,
-  `id_area` varchar(45) DEFAULT NULL,
-  `likes` int(11) DEFAULT NULL,
-  `views` int(11) DEFAULT NULL,
-  `featured` tinyint(4) DEFAULT NULL,
-  `time` varchar(45) DEFAULT NULL,
-  `image` varchar(1000) DEFAULT NULL,
-  `path` varchar(45) DEFAULT NULL,
-  `neighborhood` varchar(45) DEFAULT NULL,
-  `area` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`,`id_news`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) 
+    CREATE TABLE IF NOT EXISTS profiles (
+        id_news VARCHAR(255) UNIQUE NOT NULL,
+        id_area VARCHAR(255) UNIQUE NOT NULL,
+        title VARCHAR(255),
+        author VARCHAR(255),
+        content VARCHAR(255) NOT NULL,
+        date VARCHAR(255),
+        user VARCHAR(255)
+    )
 `;
 
 module.exports = newsSchema;
