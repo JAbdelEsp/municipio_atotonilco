fc83a595ed177d49e1bf76bc3dadee78ee968bff
JAbdelEspinosa <jabdel.espinosa@gmail.com>
2025-02-04 09:17:54 -0600

    fix constants

--- dashboard/src/constants.ts
+++ dashboard/src/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = "https://191.101.15.147:9000";
+export const BACKEND_BASE_URL = "https://creativadevelopmentsolutions.com:9000";
 // PROD_URL: "http://191.101.15.147:9000";
 export type ErrorResponse = {
   message: string;
